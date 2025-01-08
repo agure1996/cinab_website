759e8bc219fe9356b62f5a79256074592af1b439
A <abbasgure@gmail.com>
2025-01-08 17:54:01 +0000

    setting up skeleton springboot app

---
+++ src/main/java/com/gure/cinab/CinabApplication.java
@@ -0,0 +1,13 @@
+package com.gure.cinab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gure/cinab/CinabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gure.cinab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
