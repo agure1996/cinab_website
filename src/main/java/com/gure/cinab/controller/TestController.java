43cbd20c0310b68535778e97cd12a88b4f0f1d55
A <abbasgure@gmail.com>
2025-01-08 21:49:53 +0000

    test

---
+++ src/main/java/com/gure/cinab/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.gure.cinab.controller;
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Request received: Welcome");
+    }
+
+
+}
